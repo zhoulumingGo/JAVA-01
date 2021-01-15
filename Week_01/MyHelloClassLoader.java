9182b56414bfb6369843559dce04895fae3083fb
Zhou Lu Ming <2374316648@qq.com>
2021-01-15 22:13:34 +0800

    week01作业

---
+++ Week_01/MyHelloClassLoader.java
@@ -0,0 +1,51 @@
+import java.io.ByteArrayOutputStream;
+import java.io.IOException;
+import java.io.InputStream;
+import java.lang.reflect.InvocationTargetException;
+import java.util.Objects;
+
+
+public class MyHelloClassLoader extends ClassLoader {
+
+    public static void main(String[] args) {
+        try {
+            Class<?> clz = new MyClassLoader().findClass("Hello");
+            clz.getMethod("hello").invoke(clz.newInstance());
+        } catch (InstantiationException | NoSuchMethodException | InvocationTargetException | ClassNotFoundException | IllegalAccessException e) {
+            e.printStackTrace();
+        }
+    }
+
+    @Override
+    protected Class<?> findClass(String name) throws ClassNotFoundException {
+        byte[] bytes = null;
+        try (InputStream inputStream = getClass().getResourceAsStream(name + ".xlass")) {
+            bytes = inputStream2byte(inputStream);
+            for (int i = 0; i < bytes.length; i++) {
+                bytes[i] = (byte) (255 - bytes[i]);
+            }
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        if (Objects.isNull(bytes)) {
+            throw new ClassNotFoundException("can not find class:" + name);
+        }
+        return defineClass(name, bytes, 0, bytes.length);
+    }
+
+    /**
+     * inputStream转byte数组
+     * @param inputStream
+     * @return
+     * @throws IOException
+     */
+    public static byte[] inputStream2byte(InputStream inputStream) throws IOException {
+        ByteArrayOutputStream byteArrayOutputStream = new ByteArrayOutputStream();
+        byte[] buff = new byte[100];
+        int rc;
+        while ((rc = inputStream.read(buff, 0, 100)) > 0) {
+            byteArrayOutputStream.write(buff, 0, rc);
+        }
+        return byteArrayOutputStream.toByteArray();
+    }
+}
--- Week_01/README.md
+++ Week_01/README.md
@@ -1,1 +1,7 @@
 学习笔记
+1、了解了Java源代码编译成class文件的过程，大致了解了常用的Java字节码。
+2、学习了Java类的加载过程并通过作业实践了一个Java类的加载，了解了三种基本的Java类加载器。
+3、学习了JVM内存结构，并通过画图加深了理解。
+4、使用G1 GC启动了网关程序
+java -Xmx1g -Xms1g -XX:-UseAdaptiveSizePolicy -XX:+UseSerialGC -jar gateway-server- 0.0.1-SNAPSHOT.jar
+并仿照课上案例分析一下JVM运行情况。
